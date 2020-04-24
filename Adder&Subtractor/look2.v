6144aa28dba0ef755bcd2a458c00296cd5f9625f
nada-adel-mohamady <39887982+nada-adel-mohamady@users.noreply.github.com>
2020-04-24 08:23:22 +0200

    fix look2

--- Adder&Subtractor/look2.v
+++ Adder&Subtractor/look2.v
@@ -47,10 +47,10 @@ assign p=a^b;//propagate
 assign g=a&b; //generate
  
 assign c[0]=cin;
-assign c[1]= g[0]|(p[0]&c[0]);
-assign c[2]= g[1] | (p[1]&g[0]) | p[1]&p[0]&c[0];
-assign c[3]= g[2] | (p[2]&g[1]) | p[2]&p[1]&g[0] | p[1]&p[1]&p[0]&c[0];
-assign cout= g[3] | (p[3]&g[2]) | p[3]&p[2]&g[1]| p[3]&p[2]&p[1]&g[0] | p[3]&p[2]&p[1]&p[0]&c[0];
+assign c[1]= g[0]|(p[0]&cin);
+assign c[2]= g[1] | (p[1]&g[0]) | p[1]&p[0]&cin;
+assign c[3]= g[2] | (p[2]&g[1]) | p[2]&p[1]&g[0] | p[2]&p[1]&p[0]&cin;
+assign cout= g[3] | (p[3]&g[2]) | p[3]&p[2]&g[1]| p[3]&p[2]&p[1]&g[0] | p[3]&p[2]&p[1]&p[0]&cin;
 assign result=p^c;
  
 endmodule
