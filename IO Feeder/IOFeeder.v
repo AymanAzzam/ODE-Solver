b0003cf16b01a49967e9fda6946f6ecc373af2ce
Nader <nader_abdelghani@hotmail.com>
2020-04-24 03:00:07 +0200

    Set Required I/O Control Signals for Data Feeding
    - This is considered a test-bench for feeding the encoded json data to the I/O module.
    - This is in the place of the required TCL script.
    - This doesn't require synthesis and doesn't synthesis as far as I know.

--- IO Feeder/IOFeeder.v
+++ IO Feeder/IOFeeder.v
@@ -1,30 +1,50 @@
-module CPU (input clk,
-            input rst,
-            input intrpt,
-            input ld,
-            output[31:0] dataBus);
+module IOFeeder (input clk,
+                 input reset,
+                 output intrpt,         // notifies the accelerator chip of a command
+                 output cmd,            // load = 1, process = 0
+                 input done,
+                 output[31:0] dataBus);
     
     integer dataFile;
     integer scanFile;
     reg [31:0] inputData;
+    reg intrptSignal, cmdSignal;
     
-    initial begin
-        dataFile = $fopen("output.txt", "rb");
-        if (dataFile == 0) begin
-            $display("ERROR: dataFile handle is NULL");
-            $finish;
-        end
-    end
-    
-    always @(posedge clk) begin
-        scanFile = $fscanf(dataFile, "%b", inputData);
-        if (!$feof(dataFile)) begin
-            $display("%b",inputData);
+    always @(posedge clk, posedge reset) begin
+        if (reset) begin
+            dataFile = $fopen("output.txt", "rb");
+            if (dataFile == 0) begin
+                $display("ERROR: dataFile handle is NULL");
+                $finish;
+            end
+            scanFile <= $fscanf(dataFile, "%b", inputData);
+            // Send load command + interrupt signal to notify the chip to receive a data packet
+            intrptSignal  <= 1;
+            cmdSignal     <= 1;
         end
         else begin
-            $fclose(dataFile);
-            $finish;
+            if (dataFile) begin
+                if (!$feof(dataFile)) begin
+                    if (done && intrptSignal) begin
+                        scanFile <= $fscanf(dataFile, "%b", inputData);
+                        // Send load command + interrupt signal to notify the chip to receive a data packet
+                        $display("scanFile = %d", scanFile);
+                        intrptSignal  <= 0;
+                    end
+                    else begin
+                        intrptSignal <= 1;
+                    end
+                end
+                else begin
+                    intrptSignal  <= 1;
+                    cmdSignal     <= 0;
+                end
+            end
         end
     end
-    
+
+    assign dataBus = inputData;
+    assign intrpt = intrptSignal;
+    assign cmd = cmdSignal;
+
 endmodule
