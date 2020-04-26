5643e276b74db508b3eefab1e0078be8c81530fc
Nader <nader_abdelghani@hotmail.com>
2020-04-26 10:32:05 +0200

    Receive and Save Result Data from I/O Module

--- IO Feeder/IOFeeder.v
+++ IO Feeder/IOFeeder.v
@@ -1,17 +1,28 @@
 module IOFeeder (input clk,
                  input reset,
-                 output intrpt,         // notifies the accelerator chip of a command
-                 output cmd,            // load = 1, process = 0
+                 output reg intrpt, // notifies the accelerator chip of a command
+                 output reg cmd,    // load = 1, process = 0
                  input done,
-                 output[31:0] dataBus);
+                 inout[31:0] data);
     
     integer dataFile;
+    integer resultFile;
     integer scanFile;
-    reg [31:0] inputData;
-    reg intrptSignal, cmdSignal;
+    integer N;
+    integer T;
+    integer elementsCount;
+    integer receivedDataCount;
+    integer outputtedXCount;
+    reg [31:0] fileInputData;
+    
+    assign data = dataFile ? fileInputData : 32'bz;
     
     always @(posedge clk, posedge reset) begin
         if (reset) begin
+            // Set received data count to -1
+            outputtedXCount   = 0;
+            receivedDataCount = -1;
+            elementsCount = 0;
             // Open file for reading
             dataFile = $fopen("output.txt", "rb");
             // Missing file error catching
@@ -20,39 +31,58 @@ module IOFeeder (input clk,
                 $finish;
             end
             // Prepare first data packet
-            scanFile <= $fscanf(dataFile, "%b", inputData);
+            scanFile <= $fscanf(dataFile, "%b", fileInputData);
             // Set load command control signal + interrupt signal to notify the chip to receive a data packet
-            intrptSignal <= 1;
-            cmdSignal    <= 1;
+            intrpt <= 1;
+            cmd    <= 1;
         end
         else begin
             if (dataFile) begin // Send data as long as the file is open
                 if (done) begin // Prepare a new data packet, whenever the I/O module receives a data packet
-                    scanFile <= $fscanf(dataFile, "%b", inputData);
+                    scanFile <= $fscanf(dataFile, "%b", fileInputData);
                     if (!$feof(dataFile)) begin
-                        intrptSignal <= 0; // If there are data to be sent, inform the I/O module to wait by suppressing the interrupt signal until the new data is prepared
+                        intrpt <= 0; // If there are data to be sent, inform the I/O module to wait by suppressing the interrupt signal until the new data is prepared
                     end
                     else begin // Otherwise, send 'process' command and close the file
-                        intrptSignal <= 1;
-                        cmdSignal    <= 0;
+                        intrpt <= 1;
+                        cmd    <= 0;
                         $fclose(dataFile);
                         dataFile <= 0;
                     end
                 end
                 else begin // Inform the I/O module of the new data packet on the falling edge of the 'done' signal
-                    intrptSignal <= 1;
+                    intrpt <= 1;
                 end
             end
             else begin // Receive results whenever the accelerator chip finishes data processing
-                if (done) begin
-                    // Receive data from I/O module
+                if (done) begin // Receive data from I/O module
+                    if (receivedDataCount == -1) begin
+                        T                 <= data[3:0];
+                        N                 <= data[9:4];
+                        elementsCount     <= data[3:0] * data[9:4];
+                        receivedDataCount <= receivedDataCount + 1;
+                        resultFile <= $fopen("results.txt");
+                    end
+                    else begin
+                        if (receivedDataCount < elementsCount) begin
+                            if (outputtedXCount < N) begin
+                                $fwrite(resultFile, "%b ", data);
+                                outputtedXCount   <= outputtedXCount + 1;
+                                receivedDataCount <= receivedDataCount + 1;
+                            end
+                            else begin
+                                $fwrite(resultFile, "\n");
+                                outputtedXCount <= 0;
+                            end
+                        end
+                        else begin
+                            $fclose(resultFile);
+                        end
+                    end
                 end
             end
         end
     end
     
-    assign dataBus = inputData;
-    assign intrpt  = intrptSignal;
-    assign cmd     = cmdSignal;
     
 endmodule
