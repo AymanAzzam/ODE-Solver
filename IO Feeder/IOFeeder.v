68b0c0b525a3698b578547f0a5f1653e55ab88e4
Nader <nader_abdelghani@hotmail.com>
2020-04-26 10:51:50 +0200

    Refactor IOFeeder.v

--- IO Feeder/IOFeeder.v
+++ IO Feeder/IOFeeder.v
@@ -5,49 +5,48 @@ module IOFeeder (input clk,
                  input done,
                  inout[31:0] data);
     
-    integer dataFile;
+    integer encodedDataFile;
     integer resultFile;
     integer scanFile;
     integer N;
-    integer T;
-    integer elementsCount;
-    integer receivedDataCount;
-    integer outputtedXCount;
+    integer totalElementsCount;
+    integer totalReceivedElementsCount;
+    integer currentVectorElementsCount;
     reg [31:0] fileInputData;
     
-    assign data = dataFile ? fileInputData : 32'bz;
+    assign data = encodedDataFile ? fileInputData : 32'bz;
     
     always @(posedge clk, posedge reset) begin
         if (reset) begin
             // Set received data count to -1
-            outputtedXCount   = 0;
-            receivedDataCount = -1;
-            elementsCount = 0;
+            currentVectorElementsCount   = 0;
+            totalReceivedElementsCount = -1;
+            totalElementsCount = 0;
             // Open file for reading
-            dataFile = $fopen("output.txt", "rb");
+            encodedDataFile = $fopen("output.txt", "rb");
             // Missing file error catching
-            if (dataFile == 0) begin
-                $display("ERROR: dataFile handle is NULL");
+            if (encodedDataFile == 0) begin
+                $display("ERROR: encodedDataFile handle is NULL");
                 $finish;
             end
             // Prepare first data packet
-            scanFile <= $fscanf(dataFile, "%b", fileInputData);
+            scanFile <= $fscanf(encodedDataFile, "%b", fileInputData);
             // Set load command control signal + interrupt signal to notify the chip to receive a data packet
             intrpt <= 1;
             cmd    <= 1;
         end
         else begin
-            if (dataFile) begin // Send data as long as the file is open
+            if (encodedDataFile) begin // Send data as long as the file is open
                 if (done) begin // Prepare a new data packet, whenever the I/O module receives a data packet
-                    scanFile <= $fscanf(dataFile, "%b", fileInputData);
-                    if (!$feof(dataFile)) begin
+                    scanFile <= $fscanf(encodedDataFile, "%b", fileInputData);
+                    if (!$feof(encodedDataFile)) begin
                         intrpt <= 0; // If there are data to be sent, inform the I/O module to wait by suppressing the interrupt signal until the new data is prepared
                     end
                     else begin // Otherwise, send 'process' command and close the file
                         intrpt <= 1;
                         cmd    <= 0;
-                        $fclose(dataFile);
-                        dataFile <= 0;
+                        $fclose(encodedDataFile);
+                        encodedDataFile <= 0;
                     end
                 end
                 else begin // Inform the I/O module of the new data packet on the falling edge of the 'done' signal
@@ -55,28 +54,27 @@ module IOFeeder (input clk,
                 end
             end
             else begin // Receive results whenever the accelerator chip finishes data processing
-                if (done) begin // Receive data from I/O module
-                    if (receivedDataCount == -1) begin
-                        T                 <= data[3:0];
-                        N                 <= data[9:4];
-                        elementsCount     <= data[3:0] * data[9:4];
-                        receivedDataCount <= receivedDataCount + 1;
-                        resultFile <= $fopen("results.txt");
+                if (done) begin
+                    if (totalReceivedElementsCount == -1) begin // First time receiving data
+                        N                 <= data[9:4]; // N => vector X length
+                        totalElementsCount     <= data[3:0] * data[9:4]; // Total elements = N * Time steps at which X's are required
+                        totalReceivedElementsCount <= totalReceivedElementsCount + 1; // totalReceivedElementsCount now equals 0
+                        resultFile <= $fopen("results.txt"); // Open file for writing X's
                     end
                     else begin
-                        if (receivedDataCount < elementsCount) begin
-                            if (outputtedXCount < N) begin
+                        if (totalReceivedElementsCount < totalElementsCount) begin
+                            if (currentVectorElementsCount < N) begin // Write each X vector in a row with spaces between its elements
                                 $fwrite(resultFile, "%b ", data);
-                                outputtedXCount   <= outputtedXCount + 1;
-                                receivedDataCount <= receivedDataCount + 1;
+                                currentVectorElementsCount   <= currentVectorElementsCount + 1;
+                                totalReceivedElementsCount <= totalReceivedElementsCount + 1;
                             end
                             else begin
-                                $fwrite(resultFile, "\n");
-                                outputtedXCount <= 0;
+                                $fwrite(resultFile, "\n"); // Write a newline whenever an X gets all its elements written
+                                currentVectorElementsCount <= 0;
                             end
                         end
                         else begin
-                            $fclose(resultFile);
+                            $fclose(resultFile); // Close the file when finished
                         end
                     end
                 end
